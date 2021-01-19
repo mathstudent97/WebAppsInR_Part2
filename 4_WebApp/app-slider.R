d8377dd9d566042278294619e79e696a7d0655c4
Verna Marie Maullon <59632000+mathstudent97@users.noreply.github.com>
2021-01-18 23:15:55 -0500

    Add files via upload
    This app aims to build an Iris Predictor which deploys an ML model of the Iris dataset.   
    
    This is just another version of the app shown in 'app-numeric' code.        
    Ensure to clean the environment within R after running the previous app.

---
+++ 4_WebApp/app-slider.R
@@ -0,0 +1,141 @@
+######################################
+
+# Building an ML / Data-Driven       # 
+#Web Application in R                #
+# Code Part 3                        #
+
+# This is just another version of the#
+#app shown in 'app-numeric.R'        #
+
+# So, make sure to clean the         #
+#environment after running the app   #
+#shown in the 'app-numeric' file     #
+
+# This app aims to build an          #
+#Iris Predictor which deploys an     #
+#ML model of the Iris dataset.       #
+# Again, using the Shiny Web App     # 
+#framework                           #
+
+
+# Check out uploaded pdf for more    #
+#info.                               #
+
+
+# See References in pdf              #
+
+######################################
+
+
+library(shiny)
+library(data.table)
+library(randomForest)
+
+
+# Read in the Random Forest model
+model <- readRDS("model.rds")
+
+
+# Training set
+TrainSet <- read.csv("training.csv", header = TRUE)
+TrainSet <- TrainSet[,-1]
+
+
+
+###########################
+##### User Interfrace #####
+###########################
+
+
+ui <- pageWithSidebar(
+  
+  # Page header
+  headerPanel('Iris Predictor App'),
+  
+  
+  # Input values
+  sidebarPanel(
+    HTML("<h3>Input Parameters</h3>"),
+    sliderInput("Sepal.Length", label = "Sepal Length", value = 5.0,
+                min = min(TrainSet$Sepal.Length),
+                # Want: TrainSet object and Sepal Length column
+                max = max(TrainSet$Sepal.Length)
+    ),
+    sliderInput("Sepal.Width", label = "Sepal Width", value = 3.6,
+                min = min(TrainSet$Sepal.Width),
+                max = max(TrainSet$Sepal.Width)),
+    sliderInput("Petal.Length", label = "Petal Length", value = 1.4,
+                min = min(TrainSet$Petal.Length),
+                max = max(TrainSet$Petal.Length)),
+    sliderInput("Petal.Width", label = "Petal Width", value = 0.2,
+                min = min(TrainSet$Petal.Width),
+                max = max(TrainSet$Petal.Width)),
+    
+    actionButton("submitbutton", "Submit", class = "btn btn-primary")
+  ),
+  
+  mainPanel(
+    tags$label(h3('Status/Output')), # Status/Output Text Box
+    verbatimTextOutput('contents'),
+    tableOutput('tabledata') # Prediction results table
+    
+  )
+)
+
+
+
+##################
+##### Server #####
+##################
+
+
+server<- function(input, output, session) {
+  
+  # Input Data
+  datasetInput <- reactive({  
+    
+    df <- data.frame(
+      Name = c("Sepal Length",
+               "Sepal Width",
+               "Petal Length",
+               "Petal Width"),
+      Value = as.character(c(input$Sepal.Length,
+                             input$Sepal.Width,
+                             input$Petal.Length,
+                             input$Petal.Width)),
+      stringsAsFactors = FALSE)
+    
+    Species <- 0
+    df <- rbind(df, Species)
+    input <- transpose(df)
+    write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+    
+    test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
+    
+    Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
+    print(Output)
+    
+  })
+  
+  # Status/Output Text Box
+  output$contents <- renderPrint({
+    if (input$submitbutton>0) { 
+      isolate("Calculation complete.") 
+    } else {
+      return("Server is ready for calculation.")
+    }
+  })
+  
+  # Prediction results table
+  output$tabledata <- renderTable({
+    if (input$submitbutton>0) { 
+      isolate(datasetInput()) 
+    } 
+  })
+  
+}
+
+####################################
+# Create the shiny app             #
+####################################
+shinyApp(ui = ui, server = server)
